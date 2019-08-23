4669e66390fbddaa67a79e6bc9014d4280424e3f
victoriak94 <vkamila6@gmail.com>
2019-08-23 00:19:28 -0400

    Create addTodo method in Home.vue

--- todos2/src/views/Home.vue
+++ todos2/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "../components/Todos.vue";
+import AddTodo from "../components/AddTodo.vue";
 
 export default {
   name: "home",
   components: {
-    Todos
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -36,6 +39,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 };
