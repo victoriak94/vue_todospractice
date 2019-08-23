9e2e6d78d6d25e84066d97bd43d189e337afc4ff
victoriak94 <vkamila6@gmail.com>
2019-08-22 23:49:07 -0400

    Create header component. Import header component to Home.vue. Create AddTodo component

--- todos2/src/App.vue
+++ todos2/src/App.vue
@@ -1,12 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view />
   </div>
 </template>
 
+<script>
+import Header from "./components/layout/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Helvetica, Arial, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
---
+++ todos2/src/components/AddTodo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <form>
+      <input type="text" name="title" placeholder="Add Todo..." />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
